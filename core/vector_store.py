d77f08156b7b96263e61fc796b4b970f8b255aca
Abhijeet.M <abhijeetiyer07@gmail.com>
2026-08-31 18:59:36 +0530

    Implement vector store build, load, and retriever functions

---
+++ core/vector_store.py
@@ -0,0 +1,60 @@
+import os 
+# pyrefly: ignore [missing-import]
+from langchain_chroma import Chroma 
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+CHROMA_DIR="vector_db"
+COLLECTION_NAME="meeting_transcript"
+EMBEDDING_MODEL="all-MiniLM-L6-v2"
+
+## Return embeddings 
+def get_embeddings():
+  return HuggingFaceEmbeddings(
+    model_name=EMBEDDING_MODEL,
+    model_kwargs={"device":"cpu"}
+  )
+
+
+## Build vector Store 
+def build_vector_store(transcript:str)->Chroma:
+   print("Building vector Store")
+   splitter=RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50,
+   )
+   chunks=splitter.split_text(transcript)
+   
+   docs=[
+    Document(page_content=chunk, metadata={"chunk_id":i}) 
+    for i,chunk in enumerate(chunks)
+   ]
+
+   embeddings=get_embeddings()
+   vector_store=Chroma.from_documents(
+    documents=docs,
+    embedding=embeddings,
+    collection_name=COLLECTION_NAME,
+    persist_directory=CHROMA_DIR
+   )
+  
+   return vector_store
+
+
+## Load vector store from disk  
+def load_vector_store()->Chroma:
+  embeddings=get_embeddings()
+  vector_store=Chroma(
+    collection_name=COLLECTION_NAME,
+    embedding_functions=embeddings,
+    persist_directory=CHROMA_DIR
+  )
+  return vector_store
+
+## Get Retriver to query vector db
+def get_retriever(vector_store : Chroma, k :int = 4):
+    return vector_store.as_retriever(
+        search_type = 'similarity',
+        search_kwargs = {"k":k}
+    )
